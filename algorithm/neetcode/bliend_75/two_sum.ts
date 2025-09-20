b2411f41a5bcc80e827fcb967924dd19c12136c5
JadenWe <jaden910825@gmail.com>
2025-09-20 15:01:30 -0400

    solve: two sum

---
+++ algorithm/neetcode/bliend_75/two_sum.ts
@@ -0,0 +1,41 @@
+/**
+	url: https://neetcode.io/problems/two-integer-sum?list=blind75
+	time complexity: O(n2) 
+*/
+function solution1(nums: number[], target: number) {
+	const result: number[] = [];
+	
+	for (let i = 0; i < nums.length; i ++) {
+		for (let j = i + 1; j < nums.length; j ++) {
+			if ((nums[i] + nums[j]) === target) {
+				result.push(i, j);
+			}	
+		}	
+	}
+
+	return result;
+}
+
+
+console.log(solution1([3,4,5,6], 7));
+console.log(solution1([4,5,6], 10));
+console.log(solution1([5,5], 10));
+
+// time complexity: O(n)
+function solution2(nums: number[], target: number) {
+	const result = new Map();
+
+	for (let i = 0; i < nums.length; i ++) {
+		const diff = target - nums[i];
+
+		if (result.has(diff)) {
+			return [result.get(diff), i];
+		}
+
+		result.set(nums[i], i);
+	}
+}
+
+console.log(solution2([3,4,5,6], 7));
+console.log(solution2([4,5,6], 10));
+console.log(solution2([5,5], 10));
